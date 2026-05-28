c8e960627510292fa332c820b59b30ddc1fa9b7e
hiletmis <31757771+hiletmis@users.noreply.github.com>
2026-05-28 15:30:16 +0300

    Add market configuration and validation functions to MorphoVaultV2DeployHelper

--- contracts/MorphoVaultV2DeployHelper.sol
+++ contracts/MorphoVaultV2DeployHelper.sol
@@ -9,6 +9,7 @@ import {IVaultV2Factory} from "./external/morpho/interfaces/IVaultV2Factory.sol"
 import {IVaultV2} from "./external/morpho/interfaces/IVaultV2.sol";
 import {IERC20} from "./external/morpho/interfaces/IERC20.sol";
 import {IERC4626} from "./external/morpho/interfaces/IERC4626.sol";
+import {IMorpho, MarketParams, Id, Position} from "./external/morpho/interfaces/IMorpho.sol";
 import {IMorphoMarketV1AdapterV2} from "./external/morpho/interfaces/IMorphoMarketV1AdapterV2.sol";
 import {IMorphoMarketV1AdapterV2Factory} from "./external/morpho/interfaces/IMorphoMarketV1AdapterV2Factory.sol";
 
@@ -17,6 +18,8 @@ contract MorphoVaultV2DeployHelper {
     address public immutable vaultV2FactoryAddress;
     address public immutable adapterV2FactoryAddress;
     address public immutable morphoRegistryAddress;
+
+    address constant DEAD_ADDRESS = 0x000000000000000000000000000000000000dEaD;
 
     constructor(address _vaultV2FactoryAddress, address _adapterV2FactoryAddress, address _morphoRegistryAddress) {
         vaultV2FactoryAddress = _vaultV2FactoryAddress;
@@ -33,7 +36,9 @@ contract MorphoVaultV2DeployHelper {
         address finalOwner;
         address finalCurator;      
         address allocator;
-        address sentinel;          
+        address sentinel;    
+
+        bytes32 marketID;      
         
         uint256 vaultTimelockHigh; 
         uint256 vaultTimelockMid;  
@@ -71,6 +76,7 @@ contract MorphoVaultV2DeployHelper {
         result.adapter = adapter;
 
         iVault2.submit(abi.encodeCall(IVaultV2.setIsAllocator, (p.allocator, true)));
+        iVault2.submit(abi.encodeCall(IVaultV2.setIsAllocator, (address(this), true)));
         iVault2.submit(abi.encodeCall(IVaultV2.setAdapterRegistry, (morphoRegistryAddress)));
         
         bytes memory adapterIdData = abi.encode("this", adapter);
@@ -85,6 +91,7 @@ contract MorphoVaultV2DeployHelper {
 
         iVault2.setAdapterRegistry(morphoRegistryAddress);
         iVault2.setIsAllocator(p.allocator, true);
+        iVault2.setIsAllocator(address(this), true);
         iVault2.addAdapter(adapter);
 
         iVault2.increaseAbsoluteCap(adapterIdData, type(uint128).max);
@@ -94,6 +101,13 @@ contract MorphoVaultV2DeployHelper {
         iVault2.abdicate(IVaultV2.setReceiveSharesGate.selector);
         iVault2.abdicate(IVaultV2.setSendSharesGate.selector);
         iVault2.abdicate(IVaultV2.setReceiveAssetsGate.selector);
+
+        if (p.marketID != bytes32(0)) {
+            _configureMarketAndLiquidityAdapter(vault, p.marketID, adapter, p.asset);
+        }
+
+        iVault2.submit(abi.encodeCall(IVaultV2.setIsAllocator, (address(this), false)));
+        iVault2.setIsAllocator(address(this), false);
 
         _configureVaultTimelocks(vault, p.vaultTimelockHigh, p.vaultTimelockMid);
         _configureAdapterTimelocks(adapter, p.adapterTimelock);
@@ -107,6 +121,52 @@ contract MorphoVaultV2DeployHelper {
         iVault2.setOwner(p.finalOwner);
 
         emit VaultDeployed(vault, adapter, p.finalOwner, p.finalCurator, p.allocator);
+    }
+
+    function _configureMarketAndLiquidityAdapter(address vault, bytes32 marketID, address adapter, address loanAsset) internal {
+        IVaultV2 iVault2 = IVaultV2(vault);
+        IMorphoMarketV1AdapterV2 iAdapter = IMorphoMarketV1AdapterV2(adapter);
+        
+        address morpho = iAdapter.morpho();
+        
+        MarketParams memory marketParams = IMorpho(morpho).idToMarketParams(Id.wrap(marketID));
+
+        address irm = iAdapter.adaptiveCurveIrm();
+        _validateMarketParams(marketParams, loanAsset, irm);
+
+        bytes memory liquidityData = abi.encode(marketParams);
+        iVault2.submit(abi.encodeCall(IVaultV2.setLiquidityAdapterAndData, (adapter, liquidityData)));
+        iVault2.setLiquidityAdapterAndData(address(iAdapter), liquidityData);
+
+        Position memory deadPosition = IMorpho(morpho).position(Id.wrap(marketID), address(DEAD_ADDRESS));
+        uint256 deadSupplyShares = deadPosition.supplyShares;
+
+        _validateDepositAmount(1e9, deadSupplyShares);
+
+        bytes memory collateralTokenIdData = abi.encode("collateralToken", marketParams.collateralToken);
+        iVault2.submit(abi.encodeCall(IVaultV2.increaseAbsoluteCap, (collateralTokenIdData, type(uint128).max)));
+        iVault2.submit(abi.encodeCall(IVaultV2.increaseRelativeCap, (collateralTokenIdData, 1e18)));
+        iVault2.increaseAbsoluteCap(collateralTokenIdData, type(uint128).max);
+        iVault2.increaseRelativeCap(collateralTokenIdData, 1e18);
+
+        bytes memory marketIdData = abi.encode("this/marketParams", address(adapter), marketParams);
+        iVault2.submit(abi.encodeCall(IVaultV2.increaseAbsoluteCap, (marketIdData, type(uint128).max)));
+        iVault2.submit(abi.encodeCall(IVaultV2.increaseRelativeCap, (marketIdData, 1e18)));
+        iVault2.increaseAbsoluteCap(marketIdData, type(uint128).max);
+        iVault2.increaseRelativeCap(marketIdData, 1e18);
+    }
+
+    function _validateDepositAmount(uint256 requiredDeadDeposit, uint256 deadSupplyShares) internal pure {
+        require(deadSupplyShares >= requiredDeadDeposit, "Market needs dead deposit");
+    }
+
+    function _validateMarketParams(MarketParams memory marketParams, address expectedAsset, address expectedIrm)
+        internal
+        pure
+    {
+        require(marketParams.loanToken == expectedAsset, "Market loanToken does not match vault asset");
+        require(marketParams.irm == expectedIrm, "Market IRM does not match adaptiveCurveIrm");
+        require(marketParams.collateralToken != address(0), "Market not found (collateralToken is zero)");
     }
 
     function _configureVaultTimelocks(
@@ -127,12 +187,12 @@ contract MorphoVaultV2DeployHelper {
         selectors[6] = iVault.increaseTimelock.selector; 
  
         for (uint256 i = 0; i < 4; i++) {
-            iVault.submit(abi.encodeCall(iVault.increaseTimelock, (selectors[i], midDuration)));
+            iVault.submit(abi.encodeCall(IVaultV2.increaseTimelock, (selectors[i], midDuration)));
             iVault.increaseTimelock(selectors[i], midDuration);
         }
 
         for (uint256 i = 4; i < selectors.length; i++) {
-            iVault.submit(abi.encodeCall(iVault.increaseTimelock, (selectors[i], highDuration))); 
+            iVault.submit(abi.encodeCall(IVaultV2.increaseTimelock, (selectors[i], highDuration))); 
             iVault.increaseTimelock(selectors[i], highDuration);
         }
 
@@ -152,7 +212,7 @@ contract MorphoVaultV2DeployHelper {
         selectors[3] = IMorphoMarketV1AdapterV2.increaseTimelock.selector; 
 
         for (uint256 i = 0; i < selectors.length; i++) {
-            iAdapter.submit(abi.encodeCall(iAdapter.increaseTimelock, (selectors[i], duration)));
+            iAdapter.submit(abi.encodeCall(IMorphoMarketV1AdapterV2.increaseTimelock, (selectors[i], duration)));
             iAdapter.increaseTimelock(selectors[i], duration);
         }
 
